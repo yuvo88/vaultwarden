61fd211db2b9725ca9ccadc5eaca1767eb0a80f0
Yuval Schnitzer <yuvo88@gmail.com>
2024-05-05 16:18:07 +0300

    /admin/two-factor/authenticator works

--- migrations/postgresql/2024-05-02-010000_create_twofactor_admin/up.sql
+++ migrations/postgresql/2024-05-02-010000_create_twofactor_admin/up.sql
@@ -2,5 +2,6 @@ CREATE TABLE twofactor_admin (
   uuid      CHAR(36) NOT NULL PRIMARY KEY,
   atype     INTEGER  NOT NULL,
   enabled   BOOLEAN  NOT NULL,
-  data      TEXT     NOT NULL
+  data      TEXT     NOT NULL,
+  last_used INTEGER  NOT NULL DEFAULT 0
 );
