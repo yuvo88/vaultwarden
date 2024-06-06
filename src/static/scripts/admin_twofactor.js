fdda3dcba9f9be7fb656f54d7f316169c181d890
Yuval Schnitzer <yuvo88@gmail.com>
2024-06-06 16:10:39 +0300

    cleaned up jquery code

--- src/static/scripts/admin_twofactor.js
+++ src/static/scripts/admin_twofactor.js
@@ -2,6 +2,8 @@
 /* eslint-env es2017, browser */
 /* exported BASE_URL, _post */
 
+const QR_SIZE = 87;
+
 function draw_form(enabled) {
     if (enabled) {
         $('#not-enabled-form').hide();
@@ -12,21 +14,45 @@ function draw_form(enabled) {
     }
 }
 
-function draw_qr() {
-    
-    fetch(`${BASE_URL}/admin/two-factor/get-authenticator`, {
+function fetch_post(url, data) {
+    return fetch(url, {
         method: "POST",
-        body: "",
+        body: data,
         mode: "same-origin",
         credentials: "same-origin",
         headers: { "Content-Type": "application/json" }
     }).then(resp => {
         return resp.text()
-    }).then( resp_text => {
+    })
+}
+
+function disable_authenticator() {
+    const data = {
+        Type: $('#type').val(),
+    };
+    fetch_post(`${BASE_URL}/admin/two-factor/disable`, JSON.stringify(data)).then( resp_text => {
+        let resp_json = JSON.parse(resp_text);
+        draw_form(resp_json.Enabled);
+    });
+}
+
+function enable_authenticator() {
+    const data = {
+        Key: $('#key').val(),
+        Token: $('#token').val(),
+    };
+    fetch_post(`${BASE_URL}/admin/two-factor/authenticator`, JSON.stringify(data)).then( resp_text => {
+        let resp_json = JSON.parse(resp_text);
+        draw_form(resp_json.Enabled);
+    });
+}
+
+function draw_qr() {
+    fetch_post(`${BASE_URL}/admin/two-factor/get-authenticator`, "").then( resp_text => {
         let resp_json = JSON.parse(resp_text);
         let key = resp_json.Key;
         draw_form(resp_json.Enabled);
-        $('.qr_code').qrcode({"render": "image","size": 87,"text": `otpauth://totp/Vaultwarden:admin?secret=${key}&issuer=Vaultwarden`});
+        $('.qr_code').qrcode({"render": "image","size": QR_SIZE,"text": `otpauth://totp/Vaultwarden:admin?secret=${key}&issuer=Vaultwarden`});
         $('.qr_code').find('table')[0].classList.add('m-auto');
 
         $('#authenticator_code').text(key);
@@ -39,39 +65,11 @@ document.addEventListener("DOMContentLoaded", (/*event*/) => {
     draw_qr();
     $('#not-enabled-form').on('submit', (event) => {
         event.preventDefault();
-        const data = {
-            Key: $('#key').val(),
-            Token: $('#token').val(),
-        };
-        fetch(`${BASE_URL}/admin/two-factor/authenticator`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            mode: "same-origin",
-            credentials: "same-origin",
-            headers: { "Content-Type": "application/json" },
-        }).then(resp => {
-            return resp.text()
-        }).then( resp_text => {
-            let resp_json = JSON.parse(resp_text);
-            draw_form(resp_json.Enabled);
-        });
+        enable_authenticator();
+        
     });
     $('#enabled-form').on('submit', (event) => {
         event.preventDefault();
-        const data = {
-            Type: $('#type').val(),
-        };
-        fetch(`${BASE_URL}/admin/two-factor/disable`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            mode: "same-origin",
-            credentials: "same-origin",
-            headers: { "Content-Type": "application/json" },
-        }).then(resp => {
-            return resp.text()
-        }).then( resp_text => {
-            let resp_json = JSON.parse(resp_text);
-            draw_form(resp_json.Enabled);
-        });
+        disable_authenticator();
     });
 });
